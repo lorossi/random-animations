5666dd68ff164ee6cbc3cba616fe4321135f538f
Lorenzo Rossi <mail@lorenzoros.si>
2023-10-10 17:56:59 +0200

    changed sytle

--- circle-lines/js/sketch.js
+++ circle-lines/js/sketch.js
@@ -3,33 +3,32 @@ import { XOR128 } from "./xor128.js";
 
 class Sketch extends Engine {
   preload() {
-    this._duration = 900;
-    this._recording = false;
-
     this._cols = 12;
     this._scl = 0.9;
     this._circle_scl = 0.8;
-    this._texture_scl = 100;
+    this._texture_scl = 4;
+    this._noise_scl = 0.025;
+    this._lines_num = 30;
+
+    this._white = new Color(245, 245, 245);
+    this._black = new Color(15, 15, 15);
+    this._red = new Color(220, 0, 0);
+    this._text_color = new Color(0, 0, 0, 0.75);
   }
 
   setup() {
     const seed = Date.now();
     this._xor128 = new XOR128(seed);
-
-    if (this._recording) {
-      this.startRecording();
-      console.log("%cRecording started", "color:green");
-    }
+    this._noise = new SimplexNoise(seed);
   }
 
   draw() {
     this.noLoop();
 
-    const t = (this.frameCount / this._duration) % 1;
     const scl = Math.min(this.width, this.height) / this._cols;
 
     this.ctx.save();
-    this.background("red");
+    this.background(this._red.rgba);
 
     this.ctx.translate(this.width / 2, this.height / 2);
     this.ctx.scale(this._scl, this._scl);
@@ -37,26 +36,32 @@ class Sketch extends Engine {
 
     for (let x = 0; x < this._cols; x++) {
       for (let y = 0; y < this._cols; y++) {
-        const dist = (x ** 2 + y ** 2) ** 0.5 / ((this._cols * Math.SQRT2) / 2);
-        const n = this._polyEaseIn(dist, 2);
-        const lines = Math.floor(n * 15);
+        if (y == 0 && x == 0) continue;
+        if (y == this._cols - 1 && x == this._cols - 1) continue;
+
+        const dist = (x + y) / (2 * (this._cols - 1.5));
+        const lines = Math.floor(
+          this._polyEaseInOut(dist, 4) * this._lines_num
+        );
 
         this.ctx.save();
         this.ctx.translate(x * scl, y * scl);
         this.ctx.scale(this._circle_scl, this._circle_scl);
         this.ctx.lineWidth = 1;
-        this.ctx.strokeStyle = "rgb(15, 15, 15)";
-        this.ctx.beginPath();
 
-        for (let i = 0; i < lines; i++) {
+        for (let i = 0; i < this._lines_num; i++) {
           const theta_1 = this._xor128.random(Math.PI * 2);
           const theta_2 = this._xor128.random(Math.PI * 2);
+
+          this.ctx.strokeStyle =
+            i < lines ? this._black.rgba : this._white.rgba;
 
           const x1 = (scl / 2) * Math.cos(theta_1);
           const y1 = (scl / 2) * Math.sin(theta_1);
           const x2 = (scl / 2) * Math.cos(theta_2);
           const y2 = (scl / 2) * Math.sin(theta_2);
 
+          this.ctx.beginPath();
           this.ctx.moveTo(x1, y1);
           this.ctx.lineTo(x2, y2);
           this.ctx.stroke();
@@ -65,38 +70,49 @@ class Sketch extends Engine {
       }
     }
 
+    // fill the first and last circle
+    this.ctx.fillStyle = this._white.rgba;
+    this.ctx.beginPath();
+    this.ctx.arc(0, 0, (scl / 2) * this._circle_scl, 0, Math.PI * 2);
+    this.ctx.fill();
+
+    this.ctx.fillStyle = this._black.rgba;
+    this.ctx.beginPath();
+    this.ctx.arc(
+      (this._cols - 1) * scl,
+      (this._cols - 1) * scl,
+      (scl / 2) * this._circle_scl,
+      0,
+      Math.PI * 2
+    );
+    this.ctx.fill();
     this.ctx.restore();
 
-    this.ctx.save();
     // add texture
-    this.ctx.globalCompositeOperation = "darker";
+    this.ctx.save();
+    this.ctx.globalCompositeOperation = "multiply";
 
-    for (let i = 0; i < 250; i++) {
-      const x = this._xor128.random_int(this.width);
-      const y = this._xor128.random_int(this.height);
-      const ch = this._xor128.random_int(25);
-      const a = this._xor128.random(2, 50);
-      const b = this._xor128.random(2, 50);
-      const rot = this._xor128.random(Math.PI * 2);
+    for (let x = 0; x < this.width; x += this._texture_scl) {
+      for (let y = 0; y < this.height; y += this._texture_scl) {
+        const n = this._noise.noise(x * this._noise_scl, y * this._noise_scl);
+        const ch = this._polyEaseOut((n + 1) / 2) * 128;
 
-      this.ctx.fillStyle = `rgba(${ch}, ${ch}, ${ch}, 0.05)`;
-      this.ctx.beginPath();
-      this.ctx.ellipse(x, y, a, b, rot, 0, Math.PI * 2);
-      this.ctx.fill();
+        this.ctx.fillStyle = `rgba(${ch}, ${ch}, ${ch}, 0.1)`;
+        this.ctx.beginPath();
+        this.ctx.rect(x, y, this._texture_scl, this._texture_scl);
+        this.ctx.fill();
+      }
     }
     this.ctx.restore();
-
-    if (t >= 1 && this._recording) {
-      this._recording = false;
-      this.stopRecording();
-      console.log("%cRecording stopped. Saving...", "color:yellow");
-      this.saveRecording();
-      console.log("%cRecording saved", "color:green");
-    }
   }
 
-  _polyEaseIn(x, n = 2) {
-    return Math.pow(x, n);
+  _polyEaseOut(x, n = 2) {
+    return 1 - Math.pow(1 - x, n);
+  }
+
+  _polyEaseInOut(x, n = 2) {
+    if (x < 0.5) return 0.5 * Math.pow(x * 2, n);
+    else return 1 - 0.5 * Math.pow(2 - x * 2, n);
   }
 
   click() {
