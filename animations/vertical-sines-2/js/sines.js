cd23246f1d59448c2590ed86c5b93bb9f836bc8b
Lorenzo Rossi <mail@lorenzoros.si>
2023-10-18 12:14:06 +0200

    updated old animation

--- animations/vertical-sines-2/js/sines.js
+++ animations/vertical-sines-2/js/sines.js
@@ -1,42 +1,94 @@
+import { Color } from "./engine.js";
+
 class Sine {
-  constructor(width, height, xor128) {
+  constructor(x, y, width, height) {
+    this._x = x;
+    this._y = y;
     this._width = width;
     this._height = height;
+
+    this._line_scl = 1;
+  }
+
+  initDependencies(noise, xor128) {
+    this._noise = noise;
     this._xor128 = xor128;
-
-    this._scl = 2;
-    this._noise_scl = 10;
-    this._rotation = this._xor128.random_interval(0, Math.PI / 180);
-
-    const widths = Math.floor(
-      (this._height / this._scl) * 2 * this._xor128.random(0.4, 1)
-    );
-    this._widths = new Array(Math.floor(widths))
-      .fill(0)
-      .map(() => this._xor128.random(this._width));
   }
 
-  draw(ctx, t) {
+  setAttributes(fill_ch, noise_scl, color_noise_scl, time_scl, text) {
+    this._fill_ch = fill_ch;
+    this._noise_scl = noise_scl;
+    this._color_noise_scl = color_noise_scl;
+    this._time_scl = time_scl;
+    this._text = text;
+  }
+
+  init() {
+    this._seed = this._xor128.random(1e9);
+    this._rotation = this._xor128.random_interval(0, Math.PI / 360);
+
+    // calculate the length of the text
+    const ctx = document.createElement("canvas").getContext("2d");
+    ctx.font = `${this._width}px Recoleta`;
+    this._text_width =
+      ctx.measureText(this._text).width + ctx.measureText("x").width / 4;
+
+    // the lines fill the canvas until the text
+    this._lines_num = Math.floor(
+      (this._height - this._text_width) / this._line_scl
+    );
+  }
+
+  update(t) {
+    const theta = t * Math.PI * 2;
+    const tx = (1 + Math.cos(theta)) * this._time_scl;
+    const ty = (1 + Math.sin(theta)) * this._time_scl;
+
+    this._widths = new Array(this._lines_num).fill(0).map((_, i) => {
+      const n = this._noise.noise(tx, ty, i * this._noise_scl, this._seed);
+      const w = Math.max(0, (n + 1) / 2) * this._width;
+      return w;
+    });
+
+    this._fills = new Array(this._lines_num + 1).fill(0).map((_, i) => {
+      const n = this._noise.noise(
+        tx + this._seed,
+        ty + this._seed,
+        i * this._color_noise_scl,
+        this._seed + 1000
+      );
+      const ch = (this._fill_ch * (n + 1)) / 2;
+      return Color.fromMonochrome(ch, 1);
+    });
+  }
+
+  draw(ctx) {
     ctx.save();
-    ctx.strokeStyle = "rgb(240, 240, 240)";
-    ctx.lineWidth = this._scl;
+    ctx.translate(this._x, this._y);
 
-    const current_w = Math.min(
-      Math.floor(t * this._widths.length),
-      this._widths.length
-    );
-    const current_y = (current_w * this._scl) / 2;
+    // draw sines
+    ctx.save();
 
-    const w = this._widths[current_w];
-    ctx.beginPath();
-    ctx.moveTo(-w / 2, current_y);
-    ctx.lineTo(w / 2, current_y);
-    ctx.stroke();
+    for (let i = 0; i < this._lines_num; i++) {
+      const w = this._widths[i];
+      const y = i * this._line_scl;
+      ctx.fillStyle = this._fills[i].rgba;
+      ctx.fillRect(-w / 2, y, w, this._line_scl);
+    }
     ctx.restore();
-  }
 
-  get nodes_num() {
-    return this._widths.length;
+    // draw text
+    ctx.save();
+    ctx.font = `${this._width}px Recoleta`;
+    ctx.fillStyle = this._fills[this._fills.length - 1].rgba;
+    ctx.textAlign = "left";
+    ctx.textBaseline = "middle";
+    ctx.translate(0, this._height);
+    ctx.rotate(-Math.PI / 2);
+    ctx.fillText(this._text, 0, 0);
+    ctx.restore();
+
+    ctx.restore();
   }
 
   get rotation() {
--- animations/vertical-sines-2/js/sketch.js
+++ animations/vertical-sines-2/js/sketch.js
@@ -4,90 +4,74 @@ import { Sine } from "./sines.js";
 
 class Sketch extends Engine {
   preload() {
-    this._sin_num = 10;
     this._sin_spacing = 0.8;
     this._scl = 0.8;
-    this._noise_scl = 0.1;
+    this._duration = 300;
 
-    this._duration = 120;
-    this._recording = false;
-    this._frame_offset = 0;
+    this._noise_scl = 0.05;
+    this._noise_color_scl = 0.001;
+    this._noise_time_scl = 0.5;
+
+    this._background_color = new Color(246, 238, 227);
+    this._max_sines_ch = 25;
+    this._lines = "WHAT DOES ANYTHING EVEN MEAN TO YOU?".split(" ");
   }
 
   setup() {
     const seed = new Date().getTime();
-    this._sin_width = this.width / this._sin_num;
+    const sin_num = this._lines.length;
+
+    this._sin_width = this.width / sin_num;
 
     this._xor128 = new XOR128(seed);
+    this._noise = new SimplexNoise(this._xor128);
+    this._noise.setDetail(3, 0.25);
+
     this._sin_pos = new Point(0, 0);
-
-    console.log(this._sin_pos);
-
-    this._sines = new Array(this._sin_num)
-      .fill(0)
-      .map(
-        () =>
-          new Sine(
-            this._sin_width * this._sin_spacing +
-              this._xor128.random_interval(0, 0.5) * this._sin_width,
-            this.height,
-            this._xor128
-          )
+    const w = this._sin_width * this._sin_spacing;
+    this._sines = new Array(sin_num).fill(0).map((_, i) => {
+      const x = this._sin_width * i + this._sin_width / 2;
+      const y = 0;
+      const s = new Sine(x, y, w, this.height);
+      const t = i < this._lines.length ? this._lines[i] : "";
+      s.initDependencies(this._noise, this._xor128);
+      s.setAttributes(
+        this._max_sines_ch,
+        this._noise_scl,
+        this._noise_color_scl,
+        this._noise_time_scl,
+        t
       );
+      s.init();
+      return s;
+    });
 
     this._max_nodes = this._sines.reduce(
       (acc, sine) => (sine.nodes_num > acc ? sine.nodes_num : acc),
       0
     );
-
-    if (this._recording) {
-      this.startRecording();
-      console.log("%cRecording started", "color:green");
-    }
-
-    this.background("rgb(15, 15, 15)");
   }
 
   draw() {
-    const t = ((this.frameCount - this._frame_offset) / this._duration) % 1;
+    const t = (this.frameCount / this._duration) % 1;
 
     this.ctx.save();
+    this.background(this._background_color.rgb);
     this.ctx.translate(this.width / 2, this.height / 2);
     this.ctx.scale(this._scl, this._scl);
     this.ctx.translate(-this.width / 2, -this.height / 2);
 
     this._sines.forEach((sine, i) => {
-      const sine_t = t / (sine.nodes_num / this._max_nodes);
-      this.ctx.save();
-      this.ctx.translate(this.width / 2, this.height / 2);
-      this.ctx.rotate((Math.PI / 2) * i);
-      this.ctx.translate(-this.width / 2, -this.height / 2);
-
-      this.ctx.translate(i * this._sin_width + this._sin_width / 2, 0);
-      this.ctx.rotate(sine.rotation);
-      sine.draw(this.ctx, sine_t);
-      this.ctx.restore();
+      sine.update(t);
+      sine.draw(this.ctx, t);
     });
 
     this.ctx.restore();
-
-    if (t == 0 && this.frameCount - this._frame_offset > 0) {
-      if (this._recording) {
-        this._recording = false;
-        this.stopRecording();
-        console.log("%cRecording stopped. Saving...", "color:yellow");
-        this.saveRecording();
-        console.log("%cRecording saved", "color:green");
-      }
-
-      this.noLoop();
-    }
   }
 
   click() {
-    this._frame_offset = this.frameCount;
     this.setup();
-    this.loop();
+    this.draw();
   }
 }
 
