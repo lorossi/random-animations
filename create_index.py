7d7bf1d3850ea570aa145e9c822529a3a855db3d
Lorenzo Rossi <mail@lorenzoros.si>
2023-11-13 12:30:55 +0100

    new index generation system

--- create_index.py
+++ create_index.py
@@ -14,31 +14,31 @@ def create_urls() -> list[tuple[str, str]]:
     )  # this is a one-liner, but at what cost?
 
 
-def spacing(n: int) -> str:
+def indent(n: int) -> str:
     """Return n tabs."""
     return "\t" * n
 
 
-def embed_urls(urls: list[str], container_class="animations-list") -> None:
+def base_indent(content: str) -> int:
+    """Return the number of tabs before the first non-empty line."""
+    return (len(content) - len(content.lstrip())) // 3
+
+
+def embed_urls(urls: list[str]) -> None:
     """Embed the urls in the page."""
-    with open("index.html") as f:
+    with open("index_template.html") as f:
         content = f.read()
 
-    new_content = f"\n{spacing(4)}<ul>"
+    links_line = [f for f in content.split("\n") if "{{ LINKS }}" in f][0]
+    base = base_indent(links_line)
+
+    new_content = ""
     for name, url in urls:
         new_content += "\n"
-        new_content += f'{spacing(5)}<li><a href="{url}">{name}</a></li>'
-    new_content += f"\n{spacing(4)}</ul>\n{spacing(3)}"
+        new_content += f'{indent(base+1)}<a href="{url}">{name}</a>'
+    new_content += f"\n{indent(base)}"
 
-    container_str = f'<div class="{container_class}">'
-    container_start = content.find(container_str)
-    container_end = content.find("</div>")
-
-    content = (
-        content[: container_start + len(container_str)]
-        + new_content
-        + content[container_end:]
-    )
+    content = content.replace("{{ LINKS }}", new_content)
 
     with open("index.html", "w") as f:
         f.write(content)
