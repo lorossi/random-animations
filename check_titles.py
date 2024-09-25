d6729f3a63d6651ea786c31f0bd223fefa33cb5e
Lorenzo Rossi <mail@lorenzoros.si>
2024-09-25 14:21:14 +0200

    code improved

--- check_titles.py
+++ check_titles.py
@@ -1,26 +1,35 @@
 """This script checks for titles and description in all the files called index.html."""
 
-import os
 import re
 from glob import glob
 
 
-def check_default_title(content: str) -> bool:
-    """Check if the default is present."""
-    return "<title>TEMPLATE</title>" in content
-
-
-def check_default_content(content: str) -> bool:
-    """Check if the default is present."""
-    return 'content="TEMPLATE"' in content
-
-
-def check_page_title(content: str, folder: str) -> bool:
+def check_page_title(title: str, folder: str) -> bool:
     """Check if the page title is consistent with the folder name."""
     clean_folder = folder.split("/")[-1].replace("-", " ").lower()
-    page_title = re.search(r"<title>(.*?)</title>", content).group(1)
-    clean_title = page_title.replace("-", " ").lower()
-    return clean_title in clean_folder, clean_folder, clean_title
+    clean_title = title.replace("-", " ").lower()
+    return clean_folder == clean_title
+
+
+def check_page_description(description: str, folder: str) -> bool:
+    """Check if the page description is consistent with the folder name."""
+    clean_folder = folder.split("/")[-1].replace("-", " ").lower()
+    clean_description = description.replace("-", " ").lower()
+    return clean_folder == clean_description
+
+
+def get_folder(file: str) -> str:
+    return file.split("/")[-2]
+
+
+def get_title(content: str) -> str:
+    """Get the title of the page."""
+    return re.search(r"<title>(.*?)</title>", content).group(1)
+
+
+def get_description(content: str) -> str:
+    """Get the description of the page."""
+    return re.search(r'meta name="description" content="(.*?)"', content).group(1)
 
 
 def list_files() -> list[str]:
@@ -38,23 +47,20 @@ def main():
     """Script entry point."""
     for file in list_files():
         content = open_file(file)
-        folder = os.path.dirname(file)
 
-        default_title = check_default_title(content)
-        default_content = check_default_content(content)
-        title_valid, folder_title, page_title = check_page_title(content, folder)
+        folder = get_folder(file)
+        title = get_title(content)
+        description = get_description(content)
 
-        if any([default_title, default_content, not title_valid]):
-            print(f"{file}:")
-            if default_title:
-                print("\tTitle not changed")
-            if default_content:
-                print("\tDescription not changed")
-            if not title_valid and not (default_title or default_content):
-                print(
-                    f"\tTitle not consistent with folder name: "
-                    f"page title: {page_title}, folder title: {folder_title}"
-                )
+        if title == "TEMPLATE":
+            print(f"{file}: Title not changed")
+        elif not check_page_title(title, folder):
+            print(f"{file}: Title not consistent with folder name")
+
+        if description == "TEMPLATE":
+            print(f"{file}: Description not changed")
+        elif not check_page_description(description, folder):
+            print(f"{file}: Description not consistent with folder name")
 
 
 if __name__ == "__main__":
