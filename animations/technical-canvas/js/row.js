2bdf717be9f4b21ca916259b384ffa642d1868d9
Lorenzo Rossi <mail@lorenzoros.si>
2025-08-24 19:18:11 +0200

    Added new animation

---
+++ animations/technical-canvas/js/row.js
@@ -0,0 +1,124 @@
+import { Color, SimplexNoise } from "./engine.js";
+import { XOR128 } from "./xor128.js";
+
+class Row {
+  constructor(y, width, height, seed, color) {
+    this._y = y;
+    this._width = width;
+    this._height = height;
+    this._seed = seed;
+    this._color = color;
+
+    this._xor128 = new XOR128(this._seed);
+    const noise_seed = this._xor128.random_int(1e3);
+    this._noise = new SimplexNoise(noise_seed);
+
+    this._generateCols();
+  }
+
+  _generateCols() {
+    this._col_num = this._xor128.random_int(1, 4);
+    this._cols_width = new Array(this._col_num).fill(0).map(() => {
+      return this._xor128.random_int(5, 20);
+    });
+    const columns_sum = this._cols_width.reduce((a, b) => a + b, 0);
+    this._cols_width = this._cols_width.map(
+      (w) => (w / columns_sum) * this._width
+    );
+    this._cols_x = [0];
+    for (let i = 1; i < this._col_num; i++) {
+      this._cols_x[i] = this._cols_x[i - 1] + this._cols_width[i - 1];
+    }
+
+    this._cols = new Array(this._col_num).fill(0).map((_, i) => {
+      const spacing = this._xor128.random_int(5, 20);
+      const line_width = this._xor128.random_int(3, 5);
+      const offset_x = this._xor128.random_interval(0, line_width * 3);
+      const offset_y = this._xor128.random_interval(0, line_width * 3);
+      const offset_h = this._xor128.random_interval(0, this._height / 10);
+      const rotation = this._xor128.random_interval(0, Math.PI / 240);
+      const fade = this._xor128.random(0.9, 1);
+      const color_copy = this._color.copy();
+      color_copy.lighten(fade);
+
+      const col = new Col(
+        this._cols_x[i] + offset_x,
+        this._y + offset_y,
+        this._cols_width[i],
+        this._height + offset_h,
+        spacing,
+        line_width,
+        rotation,
+        color_copy
+      );
+
+      const n = this._noise.noise(this._y * 0.01, this._cols_x[i] * 0.01);
+      if (n < 0) col.setEmpty();
+      else if (n > 0.65) col.setFull();
+
+      return col;
+    });
+  }
+
+  show(ctx) {
+    ctx.save();
+    this._cols.forEach((col) => col.show(ctx));
+    ctx.restore();
+  }
+}
+
+class Col {
+  constructor(x, y, width, height, spacing, line_width, rotation, color) {
+    this._x = x;
+    this._y = y;
+    this._width = width;
+    this._height = height;
+    this._spacing = spacing;
+    this._line_width = line_width;
+    this._rotation = rotation;
+    this._color = color;
+
+    this._empty = false;
+    this._full = false;
+  }
+
+  setEmpty() {
+    this._empty = true;
+  }
+
+  setFull() {
+    this._full = true;
+  }
+
+  show(ctx) {
+    if (this._empty) return;
+
+    if (this._full) this._drawRect(ctx);
+    else this._drawLines(ctx);
+  }
+  _drawRect(ctx) {
+    ctx.save();
+    ctx.fillStyle = this._color.rgba;
+    ctx.fillRect(this._x, this._y, this._width, this._height);
+    ctx.restore();
+  }
+
+  _drawLines(ctx) {
+    ctx.save();
+    ctx.translate(this._x, this._y);
+    ctx.rotate(this._rotation);
+    ctx.strokeStyle = this._color.rgba;
+    ctx.lineWidth = this._line_width;
+
+    for (let i = 0; i < this._width; i += this._spacing) {
+      ctx.beginPath();
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i, this._height);
+      ctx.stroke();
+    }
+
+    ctx.restore();
+  }
+}
+
+export { Row };
---
+++ animations/technical-canvas/js/sketch.js
@@ -0,0 +1,128 @@
+import { Engine, SimplexNoise, Point, Color } from "./engine.js";
+import { XOR128 } from "./xor128.js";
+import { Palette, PaletteFactory } from "./palette-factory.js";
+import { Row } from "./row.js";
+class Sketch extends Engine {
+  preload() {
+    this._bg = Color.fromMonochrome(235);
+    this._fg = Color.fromMonochrome(20);
+    this._scl = 0.9;
+    this._row_height_scl = 0.1;
+    this._texture_scl = 4;
+    this._texture_oversize = 1.5;
+
+    this._refresh = 60;
+  }
+
+  setup() {
+    this._seed = new Date().getTime();
+    this._xor128 = new XOR128(this._seed);
+    this._simplex = new SimplexNoise(this._xor128);
+
+    this._generateRows();
+    this._last_refresh = this.frameCount;
+
+    this._texture = this._generateTexture(
+      this._texture_scl,
+      this._texture_oversize
+    );
+  }
+
+  draw() {
+    if (this.frameCount - this._last_refresh > this._refresh) {
+      this._generateRows();
+      this._last_refresh = this.frameCount;
+    }
+
+    this.ctx.save();
+    this.background(this._bg);
+
+    this._rows.forEach((line) => line.show(this.ctx));
+
+    // draw the text
+    this.ctx.fillStyle = this._fg.rgba;
+    this.ctx.textAlign = "center";
+    this.ctx.textBaseline = "middle";
+    this.ctx.font = `bold ${90}px WarText`;
+    this.ctx.fillText(
+      "TECHNICAL CANVAS - STAND BY",
+      this.width / 2,
+      this.height / 2
+    );
+
+    // apply the texture
+    const dx = this._xor128.random_int(
+      0,
+      this.width * (this._texture_oversize - 1)
+    );
+    const dy = this._xor128.random_int(
+      0,
+      this.height * (this._texture_oversize - 1)
+    );
+    this.ctx.globalCompositeOperation = "multiply";
+    this.ctx.drawImage(
+      this._texture,
+      -dx,
+      -dy,
+      this.width * this._texture_oversize,
+      this.height * this._texture_oversize
+    );
+
+    this.ctx.restore();
+  }
+
+  click() {
+    this.setup();
+    this.draw();
+  }
+
+  _generateRows() {
+    this._rows_num = this._xor128.random_int(2, 15);
+
+    this._rows_heights = new Array(this._rows_num).fill(0).map((_, i) => {
+      const n = this._simplex.noise(i * this._row_height_scl);
+      return Math.max(0.1, (n + 1) / 2);
+    });
+
+    const height_sum = this._rows_heights.reduce((a, b) => a + b, 0);
+    this._rows_heights = this._rows_heights.map(
+      (h) => (h / height_sum) * this.height
+    );
+    this._lines_y = [0];
+    for (let i = 1; i < this._rows_num; i++) {
+      this._lines_y[i] = this._lines_y[i - 1] + this._rows_heights[i - 1];
+    }
+
+    this._rows = new Array(this._rows_num).fill(0).map((_, i) => {
+      const seed = this._xor128.random_int(1e9);
+      return new Row(
+        this._lines_y[i],
+        this.width,
+        this._rows_heights[i],
+        seed,
+        this._fg
+      );
+    });
+  }
+
+  _generateTexture(texture_scl = 2, oversize = 4) {
+    this._texture = new OffscreenCanvas(
+      this.width * oversize,
+      this.height * oversize
+    );
+    const ctx = this._texture.getContext("2d");
+
+    for (let x = 0; x < this._texture.width; x += texture_scl) {
+      for (let y = 0; y < this._texture.height; y += texture_scl) {
+        const ch = this._xor128.random_int(0, 255);
+        const c = Color.fromMonochrome(ch, 0.1);
+        ctx.fillStyle = c.rgba;
+        ctx.fillRect(x, y, texture_scl, texture_scl);
+      }
+    }
+
+    return this._texture;
+  }
+}
+
+export { Sketch };
